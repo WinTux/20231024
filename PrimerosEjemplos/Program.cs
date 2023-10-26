e7df9ddb433e9ae03eb059e44c66eaebf5d19f85
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2023-10-25 20:25:50 -0400

    Agregar archivos de proyecto.

---
+++ PrimerosEjemplos/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PrimerosEjemplos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, \nWorld\t!");
+            Console.WriteLine(777);//entero (integer -> int)
+            Console.WriteLine(80.50);//doble (double)
+            Console.WriteLine(-70.50f);//flotante (float)
+            Console.WriteLine(4+8);
+            Console.WriteLine((4 + 8)* 7 / 5.0);
+            Console.WriteLine(true);
+            Console.WriteLine(4 > 8);
+            Console.WriteLine(4 <= 8 && (5 == 7 || 5 <= 10));
+        }
+    }
+}
